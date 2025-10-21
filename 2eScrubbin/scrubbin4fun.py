733f505b09c01c2df4bdeae48fb46b12c3ccb24e
PotatoDono-Code <cteritter@gmail.com>
2025-10-20 21:52:34 -0400

    Messy messy messy and bad. Not working as intended, needs more work

--- 2eScrubbin/scrubbin4fun.py
+++ 2eScrubbin/scrubbin4fun.py
@@ -29,10 +29,22 @@ git_cmd.checkout('release')
 
 # -- Search through a file and pull values from nested targets
 def nested_value_search(file, keys):
+    
     for i in keys:
         if isinstance(file, dict):
             file = file.get(i)
     return file if file else None
+
+def multi_value_return(file, keys):
+    values_collection = []
+    
+    for i in keys:
+        for j in keys[i]:
+            if isinstance(file, dict):
+                file = file.get(j)
+        values_collection.append(file) if file else None
+    
+    return values_collection
 
 # -- Recurse through directory checking each .json file
 def pull_all_cat_values(file_directory, search_key):
@@ -46,7 +58,7 @@ def pull_all_cat_values(file_directory, search_key):
         try:
             with open(path, "r", encoding = "UTF-8") as file:
                 data = json.load(file)
-                captured_value = nested_value_search(data, search_key)
+                captured_value = multi_value_return(data, search_key)
                 if isinstance(captured_value, str):
                     cat_values.append(captured_value)
 
@@ -54,8 +66,15 @@ def pull_all_cat_values(file_directory, search_key):
 
             print(f"Skipped {path} due to error: {error}")
     
-    return [v for v in cat_values if v is not None]
+    rdf = pd.DataFrame(cat_values)
+    key_conversion = []
+    for i in search_key:
+        if isinstance(search_key, str):
+            search_key = [search_key]
+        key_conversion.append(search_key)
+    target_names = [last[-1] for last in key_conversion]
+    rdf.columns = target_names
 
-all_types = pd.DataFrame({"Type": pull_all_cat_values("2e Datasets/packs", ["type"])})
-unique_types = all_types["Type"].unique()
-print(unique_types)
+    return rdf
+
+all_types = pull_all_cat_values("2e Datasets/packs", ["_id", "type", ["system", "details", "publication", "title"]])
