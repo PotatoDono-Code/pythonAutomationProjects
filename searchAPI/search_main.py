5177dc813352c096a9d011db6f4ea1fb7ee2a740
PotatoDono-Code <cteritter@gmail.com>
2025-11-04 11:44:46 -0500

    Added commands to filter by numerous spell details. After testing, the call wasn't working and was getting hung up. Looking into potential causes and solutions.

--- searchAPI/search_main.py
+++ searchAPI/search_main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import pandas as pd
+import json
 
 app = FastAPI()
-df = pd.read_pickle("2eScrubbin/2e_master_pickle.pkl")
+df = pd.read_pickle("../2eScrubbin/2e_master_pickle.pkl")
 
-def filter_by_value(input_df, key = str, value = str):
-    filtered_df = input_df[input_df[key].str.lower() == value.lower()]
+def filter_by_value(input_df, key = str, search_value = str):
+    filtered_df = input_df[input_df[key].str.lower() == search_value.lower()]
     filtered_df = filtered_df.dropna(axis = 1, how = 'all')
     return filtered_df
 
@@ -15,9 +16,9 @@ def read_root():
 
 @app.get("/all_type")
 def return_all_by_type(value:str):
-    if value in df['type']:
-        result = filter_by_value(df, 'type', value)
-        return result.to_dict(orient='records')
+    if value in df['type'].unique():
+        result = filter_by_value(df, 'type', value).to_json(orient = 'records')
+        return json.loads(result)
     else:
         return {"error": "No entries matching that type"}
 
@@ -26,8 +27,8 @@ def return_all_by_type(value:str):
 # system.trait.value - spell traits (used to identify cantrip as well)
 # system.level.value - spell level
 @app.get("/spell_filter")
-def complete_spell_filter(list: str = None, level: int = None, trait: str = None, rare: str == None, save: str = None):
-    query_df = df.clone()
+def complete_spell_filter(list: str = None, level: int = None, trait: str = None, spell_rarity: str = None, save: str = None):
+    query_df = df.copy()
     
     if list:
         query_df = query_df[query_df['system.traits.traditions'] == list]
@@ -42,7 +43,11 @@ def complete_spell_filter(list: str = None, level: int = None, trait: str = None
             return {"error": "Invalid Spell Level Entry"}
     if trait:
         query_df = query_df[trait in query_df['system.traits.value']]
-    if rare:
-        query_df = query_df[query_df['system.traits.rarity'] == rare]
+    if spell_rarity:
+        query_df = query_df[query_df['system.traits.rarity'] == spell_rarity]
     if save:
         query_df = query_df[query_df['system.defense.save.statistic'] == save]
+
+    query_df = query_df.dropna(axis = 1, how = "all").to_json(orient = "records")
+
+    return json.loads(query_df)
