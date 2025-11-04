f205c3524ce3b34c27e303184324dca978023b55
PotatoDono-Code <cteritter@gmail.com>
2025-11-03 21:22:08 -0500

    Added API calls to return all values by type, and a second call to retrieve spells with various initial filters to heavily sort out the information.

--- searchAPI/search_main.py
+++ searchAPI/search_main.py
@@ -7,7 +7,7 @@ df = pd.read_pickle("2eScrubbin/2e_master_pickle.pkl")
 def filter_by_value(input_df, key = str, value = str):
     filtered_df = input_df[input_df[key].str.lower() == value.lower()]
     filtered_df = filtered_df.dropna(axis = 1, how = 'all')
-    return filtered_data
+    return filtered_df
 
 @app.get("/")
 def read_root():
@@ -15,9 +15,34 @@ def read_root():
 
 @app.get("/all_type")
 def return_all_by_type(value:str):
-    if value.isin(df['type']):
+    if value in df['type']:
         result = filter_by_value(df, 'type', value)
         return result.to_dict(orient='records')
     else:
-        {"error": "No entries matching that type"}
+        return {"error": "No entries matching that type"}
 
+# system.defense.save.statistic - save type
+# system.trait.tradition - spell tradition
+# system.trait.value - spell traits (used to identify cantrip as well)
+# system.level.value - spell level
+@app.get("/spell_filter")
+def complete_spell_filter(list: str = None, level: int = None, trait: str = None, rare: str == None, save: str = None):
+    query_df = df.clone()
+    
+    if list:
+        query_df = query_df[query_df['system.traits.traditions'] == list]
+    if level:
+        if level > 1:
+            query_df = query_df[query_df['system.level.value'] == level]
+        elif level == 1:
+            query_df = query_df[query_df['systen.level.value'] == level & "cantrip" not in query_df['system.traits.value']]
+        elif level == 0:
+            query_df = query_df["cantrip" in query_df['system.traits.value']]
+        else:
+            return {"error": "Invalid Spell Level Entry"}
+    if trait:
+        query_df = query_df[trait in query_df['system.traits.value']]
+    if rare:
+        query_df = query_df[query_df['system.traits.rarity'] == rare]
+    if save:
+        query_df = query_df[query_df['system.defense.save.statistic'] == save]
