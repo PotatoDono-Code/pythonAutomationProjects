fc663cc1868aa8cc4006c7e4227d4a806118760a
PotatoDono-Code <cteritter@gmail.com>
2025-11-06 21:50:40 -0500

    Created tests and exports to determine needed data and columns, specifically for spells. Created a filter to cut down spell columbs to Under 30.

--- 2eScrubbin/scrubbin4fun.py
+++ 2eScrubbin/scrubbin4fun.py
@@ -2,6 +2,7 @@ from git import Repo
 import os
 import pandas as pd
 import json
+import orjson
 import glob
 from collections import Counter
 from pathlib import Path
@@ -43,14 +44,17 @@ def json_to_pickle(file_directory):
     json_files = glob.glob(os.path.join(file_directory, "**/*.json"), recursive = True)
     file_count = len(json_files)
 
+    # Storage for updating df at the end
+    new_records = []
+
     # Check through every file in the directory. If the _id matches an _id in the known_files, skip it. Otherwise, load the 
     # file, convert it into a dataframe, and add to the master df. Iterate i each time and report every 500 files on progress
     for i, file_path in enumerate(json_files, 1):
         try:
-            with open(file_path, "r", encoding = "UTF-8") as file:
-                id_check = json.load(file)
+            with open(file_path, "r") as file:
+                id_check = orjson.load(file.read())
                 if id_check['_id'] not in known_files:
-                    df = pd.concat([df, pd.json_normalize(id_check)], ignore_index = True, sort = False)
+                    new_records.append(pd.json_normalize(id_check))
                     known_files.add(id_check['_id'])
                     updated += 1
                 
@@ -61,7 +65,8 @@ def json_to_pickle(file_directory):
             print(f"{file_path} failed with {e}")
 
     # If any values have been updated, rewrite the file and report the number of updates. If not, report nothing changed
-    if updated > 0:
+    if new_records:
+        df = pd.concat([df, *new_records], ignore_index = True, sort = False)
         df.to_pickle(master_pickle_path)
         print(f"Updated {updated} file entries.")
     
@@ -89,6 +94,17 @@ def dfs_by_key_values(input_df, target_key):
         print(f"Completed DF for {value}")
     return df_collection
 
+# -- Data Optimization and sorting
+def sort_common_fields(idf):
+
+    null_value = idf.notna().sum().sort_values(ascending = False)
+    null_ratio = (null_value/len(idf)).sort_values(ascending = False)
+
+    sort_table = pd.DataFrame({"Contain Value" : null_value, "Ratio" : null_ratio})
+    sort_table = sort_table.reset_index().rename(columns={"index": "column_name"})
+
+    return sort_table
+
 # -- Create a dataframe to hold future dataframes
 df_collection = {}
 
@@ -97,6 +113,15 @@ df = pd.read_pickle("2eScrubbin/2e_master_pickle.pkl")
 
 # -- Pull seperate dfs for each type
 df_collection = dfs_by_key_values(df, 'type')
-df_npc_cr = dfs_by_key_values(df_collection['npc'], 'system.details.level.value')
 
-print (df_collection['kit'])
+spell_columns = ['system.target.value', 'system.requirements', 'system.description.value', 'system.level.value', '_id', 'name', 
+                 'system.traits.rarity', 'system.counteraction', 'system.traits.value', 'system.duration.sustained', 
+                 'system.duration.value', 'system.range.value', 'type', 'system.cost.value', 'system.traits.traditions', 
+                 'system.time.value', 'system.defense.save.statistic', 'system.defense.save.basic', 'system.area.type', 
+                 'system.area.value', 'system.area.details']
+
+# sort_common_fields(df_collection['spell']).to_csv("spell_common_fields.csv", index = False)
+# print(df[df['_id'] == "YLzufF5UKRGjT83M"].dropna(axis = 1, how = "all"))
+spell_collection = df_collection['spell'][spell_columns]
+
+spell_collection.to_csv("spell_export.csv", index = False)
